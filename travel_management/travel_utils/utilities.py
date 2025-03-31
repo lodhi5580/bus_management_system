3c504cbe3e4f73689e4c30c3574a3c6215741cfe
Mohit-majumdar <majumdarmohit12345@gmail.com>
2025-03-31 23:59:08 +0530

    Added: utils

--- travel_management/travel_utils/utilities.py
+++ travel_management/travel_utils/utilities.py
@@ -1,3 +1,7 @@
 def calculate_fare(distance, price_per_km):
     """Calculate travel fare based on distance."""
     return distance * price_per_km
+
+
+def calculate_route_fare(no_seats, price):
+    return no_seats * price
